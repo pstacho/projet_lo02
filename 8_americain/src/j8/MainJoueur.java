3e528de6dd073daf09c607bbc41c866fc0421291
maxim <maxim@10.18.9.142>
2017-11-20 17:46:31 +0100

    Joueur

--- 8_americain/src/j8/MainJoueur.java
+++ 8_americain/src/j8/MainJoueur.java
@@ -1,9 +1,16 @@
 package j8;
 
 public class MainJoueur {
-
-	public MainJoueur() {
+	
+	private String nom;
+	private int nbCartes;
+	private int point;
+	
+	public MainJoueur(String name,int nbCart,int poi) {
 		// TODO Auto-generated constructor stub
+		this.nom=nom;
+		this.nbCartes=nbCart;
+		this.point=poi;
 	}
 
 }
---
+++ 8_americain/src/j8/Variante.java
@@ -0,0 +1,2 @@
+package j8;
+
