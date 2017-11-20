5ebf96cd0cd5f313a966464908085801fd3841f2
maxim <maxim@10.18.9.142>
2017-11-20 17:10:03 +0100

    Création des cartes

--- 8_americain/src/j8/PaquetCarte.java
+++ 8_americain/src/j8/PaquetCarte.java
@@ -1,15 +1,27 @@
 package j8;
-
+import java.util.ArrayList;
+//création du paquet de carte
 public class PaquetCarte {
 	
-	
+	protected ArrayList<Carte> jeuDeCartes = new ArrayList<Carte>();	
 
 	public PaquetCarte() {
 		// TODO Auto-generated constructor stub
+		
 		String[] couleur = new String[] {"Pic","Trèfle","Coeur","Carreau"};
 		String[] valeur = new String[] {"Deux","Trois","Quatre","Cinq","Six","Sept","Huit","Neuf","Dix","Valet","Dame","Roi","As"};
 		
-		for ()
+		for(int i=0; i<couleur.length; i++) {
+			for (int j=0; j<valeur.length-1; j++) {
+				Carte carte = new Carte(couleur[i],valeur[j]);
+				this.jeuDeCartes.add(carte);
+				System.out.println(this);
+				}
+			}
+			
 	}
+
+		
+	
 
 }
