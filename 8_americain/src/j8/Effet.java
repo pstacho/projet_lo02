a784ce9bea6e95c486204d5963e059604bbd11d7
maxim <maxime.trinquard@hotmail.fr>
2017-12-02 15:24:37 +0100

    feij

--- 8_americain/src/j8/Effet.java
+++ 8_americain/src/j8/Effet.java
@@ -2,6 +2,11 @@ package j8;
 
 public interface Effet {
 
+	public static void checkEffet(Manche maManche) {
+		
+			
+	}
+	
 	public static void inverserSensJeu(Manche maManche) {
 		System.out.println("Le sens de jeu est inversé");
 		maManche.setSensPositif(!maManche.isSensPositif());
